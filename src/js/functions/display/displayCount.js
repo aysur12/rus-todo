6deab6b2f234f3ebc05df6ad250af89a0e79d24b
Aysur Kuchkashev <aysursabin@gmail.com>
2022-12-29 16:37:46 +0700

    v3

--- src/js/functions/display/displayCount.js
+++ src/js/functions/display/displayCount.js
@@ -3,9 +3,6 @@ import filterNotCheckedTodo from '../filter/filterNotCheckedTodo';
 const displayCount = (todoArr) => {
   const todoCountValue = document.querySelector('.js-todo__count-value');
   todoCountValue.innerHTML = filterNotCheckedTodo(todoArr);
-
-  const todoCountText = document.querySelector('.js-todo__count-text');
-  todoCountText.innerHTML = filterNotCheckedTodo(todoArr) > 1 ? 'items' : 'item';
 };
 
 export default displayCount;
